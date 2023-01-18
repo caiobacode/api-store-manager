d40f0c33dc9fd9c35e8716e40a33cf9fcc623a17
caiobacode <caioodsf@gmail.com>
2023-01-17 22:14:58 -0300

    criando endPoint POST "/sale"

--- src/services/salesService.js
+++ src/services/salesService.js
@@ -1,6 +1,12 @@
 const salesModels = require('../models/salesModels,');
+const productsModel = require('../models/productsModel');
 
 const insertSale = async (sale) => {
+  const allProducts = await productsModel.getAllProducts();
+  
+  const productExists = sale.map((s) => allProducts.some((p) => p.id === s.productId));
+  if (productExists.some((p) => !p)) return { type: 404, data: { message: 'Product not found' } };
+
   const newSaleId = await salesModels.insertSale(sale);
   sale.map(async (s) => {
     await salesModels.insertSaleProducts(newSaleId, s.productId, s.quantity);
